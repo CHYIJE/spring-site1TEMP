49c5da55048b101a636398cbfdd7abc2cf595406
ysh <dbtmdgks7897@outlook.kr>
2023-07-27 10:29:56 +0900

    d

--- src/main/java/com/example/site1/model/user/repository/UserRepository.java
+++ src/main/java/com/example/site1/model/user/repository/UserRepository.java
@@ -8,6 +8,7 @@ import com.example.site1.model.user.entity.UserEntity;
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
 
-    //
+    // 리파지토리야 유저의 idx를 줄테니까
+    // 해당 idx를 가진 
 
 }
